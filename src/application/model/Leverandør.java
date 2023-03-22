4a5676dd778690b1cddd3494e904252d9f4ad4a3
burgerflipper01 <ande077h@gmail.com>
2023-03-22 12:02:51 +0100

    Lavede associering mellem Leverandør og Fad klassen

--- src/application/model/Leverandør.java
+++ src/application/model/Leverandør.java
@@ -1,8 +1,12 @@
 package application.model;
 
+import java.util.ArrayList;
+
 public class Leverandør {
     private String navn;
     private String land;
+
+    private ArrayList<Fad> fade = new ArrayList<>();
 
     public Leverandør(String navn, String land) {
         this.navn = navn;
@@ -16,4 +20,22 @@ public class Leverandør {
     public String getLand() {
         return land;
     }
+
+    public ArrayList<Fad> getFade() {
+        return fade;
+    }
+
+
+    public void addFad(Fad fad) {
+        if (!this.fade.contains(fad)) {
+            fade.add(fad);
+        }
+    }
+
+    public void removeFad(Fad fad) {
+        if (this.fade.contains(fad)) {
+            fade.remove(fad);
+        }
+    }
 }
+
