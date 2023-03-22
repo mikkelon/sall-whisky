47153755c8b208e7cfbb91518121a4f811c9abdd
Mads-SJ <mads.soenderstrup@gmail.com>
2023-03-22 12:16:03 +0100

    Relations between Hylde.java and Lager.java (not entirely done)

--- src/application/model/Hylde.java
+++ src/application/model/Hylde.java
@@ -2,9 +2,11 @@ package application.model;
 
 public class Hylde {
     private int hyldeNr;
+    private Lager lager;
 
-    public Hylde(int hyldeNr) {
+    public Hylde(int hyldeNr, Lager lager) {
         this.hyldeNr = hyldeNr;
+        this.lager = lager;
     }
 
     public int getHyldeNr() {
@@ -13,5 +15,22 @@ public class Hylde {
 
     public void setHyldeNr(int hyldeNr) {
         this.hyldeNr = hyldeNr;
+    }
+
+    public Lager getLager() {
+        return lager;
+    }
+
+    public void setLager(Lager lager) {
+        if (this.lager == lager) {
+            Lager oldLager = this.lager;
+            if (oldLager != null) {
+                oldLager.removeHylde(this);
+            }
+            this.lager = lager;
+            if (lager != null) {
+                lager.addHylde(this);
+            }
+        }
     }
 }
