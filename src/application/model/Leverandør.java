b6d8c4221ed254e972e84e8b260e70f02a2e0cae
burgerflipper01 <ande077h@gmail.com>
2023-03-22 11:36:02 +0100

    Lavede "Leverandør" klassen med atributter, constructer og get

---
+++ src/application/model/Leverandør.java
@@ -0,0 +1,19 @@
+package application.model;
+
+public class Leverandør {
+    private String navn;
+    private String land;
+
+    public Leverandør(String navn, String land) {
+        this.navn = navn;
+        this.land = land;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getLand() {
+        return land;
+    }
+}
