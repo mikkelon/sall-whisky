b0b289b0567960b3325285111a72ee16b5d5bb77
Mikkel <mikkelovergaardnielsen@gmail.com>
2023-03-22 11:37:16 +0100

    create Hylde.java

---
+++ src/application/model/Hylde.java
@@ -0,0 +1,17 @@
+package application.model;
+
+public class Hylde {
+    private int hyldeNr;
+
+    public Hylde(int hyldeNr) {
+        this.hyldeNr = hyldeNr;
+    }
+
+    public int getHyldeNr() {
+        return hyldeNr;
+    }
+
+    public void setHyldeNr(int hyldeNr) {
+        this.hyldeNr = hyldeNr;
+    }
+}
