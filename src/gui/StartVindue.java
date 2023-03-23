09b401d1ccd3abaafaf13d8d5703dca4b1fa3d53
Mikkel <mikkelovergaardnielsen@gmail.com>
2023-03-23 10:45:22 +0100

    opdateret StartWindow til StartVindue

--- src/gui/StartVindue.java
+++ src/gui/StartVindue.java
@@ -1,16 +1,13 @@
 package gui;
 
-import application.model.Lager;
 import gui.gui_lagerstyring.LagerstyringPane;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class StartWindow extends Application {
+public class StartVindue extends Application {
 
     @Override
     public void init() {
