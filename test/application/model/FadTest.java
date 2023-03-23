3b604bee942698f329a19ea7072b3a6dc2cb3439
Mads-SJ <mads.soenderstrup@gmail.com>
2023-03-23 12:53:29 +0100

    FadTest: 9 test cases

--- test/application/model/FadTest.java
+++ test/application/model/FadTest.java
@@ -8,18 +8,18 @@ class FadTest {
     private Fad fad;
     private Lager l1;
     private Hylde h1;
+    private FadLeverandør fl1;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
        l1 = new Lager("Baldersgade 39", "Sall Whisky Lager", 100.0);
        h1 = new Hylde(l1);
-       FadLeverandør fl1 = new FadLeverandør("Garrison Brothers", "USA");
+       fl1 = new FadLeverandør("Garrison Brothers", "USA");
        fad = new Fad(FadType.BOURBON, 80, fl1, h1);
     }
 
     @Test
-    void ConstructorTC1() {
-        FadLeverandør fl1 = new FadLeverandør("Garrison Brothers", "USA");
+    void constructorTC1() {
         fad = new Fad(FadType.BOURBON, 80, fl1, h1);
 
         assertEquals(FadType.BOURBON, fad.getFadType());
@@ -38,34 +38,40 @@ class FadTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getStørrelseILiter() {
+    void getStørrelseILiterTC3() {
+        assertEquals(80, fad.getStørrelseILiter());
     }
 
     @org.junit.jupiter.api.Test
-    void getFadNr() {
+    void getFadNrTC4() {
+        assertTrue(fad.getFadNr() > 0);
     }
 
     @org.junit.jupiter.api.Test
-    void getIndeholdtVæskeILiter() {
+    void getSetIndeholdtVæskeILiterTC5() {
+        fad.setIndeholdtVæskeILiter(50);
+        assertEquals(50, fad.getIndeholdtVæskeILiter());
     }
 
     @org.junit.jupiter.api.Test
-    void setFadType() {
+    void getHyldeTC6() {
+        assertEquals(h1, fad.getHylde());
     }
 
     @org.junit.jupiter.api.Test
-    void getHylde() {
+    void getFadLeverandørTC7() {
+        assertEquals(fl1, fad.getFadLeverandør());
     }
 
     @org.junit.jupiter.api.Test
-    void setIndeholdtVæskeILiter() {
+    void setHyldeNormalTC8() {
+        Hylde h2 = new Hylde(l1);
+        fad.setHylde(h2);
+        assertEquals(h2, fad.getHylde());
     }
 
     @org.junit.jupiter.api.Test
-    void getFadLeverandør() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void setHylde() {
+    void setHyldeFejlTC9() {
+        assertThrows(RuntimeException.class, () -> fad.setHylde(null));
     }
 }
