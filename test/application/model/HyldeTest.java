c471561396379187b6dba1d8ff701bbb999d05ea
burgerflipper01 <ande077h@gmail.com>
2023-03-23 13:05:00 +0100

    Har lavet testklasse af Hylde

--- test/application/model/HyldeTest.java
+++ test/application/model/HyldeTest.java
@@ -7,31 +7,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HyldeTest {
 
+    private Hylde hylde;
+
+    private Lager lager;
+
+    private FadLeverandør fadLeverandør;
+
+    private Fad fad;
+
     @BeforeEach
     void setUp() {
+        lager = new Lager("Baldersgade 39", "Sall Whisky Lager", 100);
+        hylde = new Hylde(lager);
+        FadLeverandør fadleverandør = new FadLeverandør("Garrison Brothers", "USA");
+        Fad fad = new Fad(FadType.BOURBON, 80, fadLeverandør, hylde);
+
+
+    }
+
+
+    @Test
+    void constructerTC1() {
+        assertEquals(hylde.getLager(), lager);
+        assertTrue(hylde.getHyldeNr() > 0);
+
+
     }
 
     @Test
     void getHyldeNr() {
+        assertTrue(hylde.getHyldeNr() > 0);
+
     }
 
     @Test
-    void getLager() {
+    void getOgSetLager() {
+        hylde.setLager(lager);
+        assertTrue(hylde.getLager().equals(lager));
+
     }
 
-    @Test
-    void setLager() {
-    }
 
     @Test
     void getFade() {
+        hylde.addFad(fad);
+        assertTrue(hylde.getFade().contains(fad));
     }
 
     @Test
     void addFad() {
+
+        hylde.addFad(fad);
+        assertTrue(hylde.getFade().contains(fad));
     }
 
     @Test
     void removeFad() {
+        hylde.addFad(fad);
+        hylde.removeFad(fad);
+        assertTrue(hylde.getFade().isEmpty());
     }
 }
