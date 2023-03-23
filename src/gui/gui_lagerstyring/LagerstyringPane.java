e44a6250aed963aec9477a77c4311e86b0c205fd
Mikkel <mikkelovergaardnielsen@gmail.com>
2023-03-23 10:18:53 +0100

    oprettet LagerstyringPane

--- src/gui/gui_lagerstyring/LagerstyringPane.java
+++ src/gui/gui_lagerstyring/LagerstyringPane.java
@@ -1,10 +1,9 @@
 package gui.gui_lagerstyring;
 
-import application.controller.Controller;
 import application.model.Fad;
 import application.model.Hylde;
 import application.model.Lager;
-import gui.BekræftSletVindue;
+import gui.OpretFad;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,7 +12,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 public class LagerstyringPane extends GridPane {
-    private Controller controller = Controller.getController();
     private ListView<Lager> lvwLagre;
     private ListView<Hylde> lvwHylder;
     private ListView<Fad> lvwFade;
@@ -28,11 +26,12 @@ public class LagerstyringPane extends GridPane {
         this.add(lblLagre, 0, 0);
 
         lvwLagre = new ListView<>();
-        lvwLagre.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> updateHylder());
         lvwLagre.setMinWidth(200);
         lvwLagre.setMaxWidth(200);
         lvwLagre.setMinHeight(400);
         this.add(lvwLagre, 0, 1);
+
+
 
         Label lblHylder = new Label("Hylder");
         this.add(lblHylder, 1, 0);
@@ -41,7 +40,6 @@ public class LagerstyringPane extends GridPane {
         lvwHylder.setMinWidth(200);
         lvwHylder.setMaxWidth(200);
         lvwHylder.setMinHeight(400);
-        lvwHylder.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> updateFade());
         this.add(lvwHylder, 1, 1);
 
         Label lblFade = new Label("Fade");
@@ -63,45 +61,18 @@ public class LagerstyringPane extends GridPane {
         Button btnSletFad = new Button("Slet Fad");
         btnSletFad.setOnAction(event -> sletFad());
         hBoxFade.getChildren().add(btnSletFad);
-
-        // #--- Opdatér listviews ---#
-        updateControls();
     }
 
     private void opretFad() {
-        OpretFadVindue window = new OpretFadVindue();
+        OpretFad window = new OpretFad();
         window.showAndWait();
     }
 
     private void sletFad() {
-        BekræftSletVindue window = new BekræftSletVindue();
-        window.showAndWait();
-        boolean valg = window.getValg();
-        if (valg)
-            System.out.println("Slet fad"); //TODO
+
     }
 
     public void updateControls() {
-        updateLagre();
-        updateHylder();
-        updateFade();
-    }
-
-    private void updateLagre() {
-        lvwLagre.getItems().setAll(controller.getLagre());
-    }
-
-    private void updateHylder() {
-        Lager valgtLager = lvwLagre.getSelectionModel().getSelectedItem();
-        if (valgtLager != null) {
-            lvwHylder.getItems().setAll(controller.getHylder(valgtLager));
-        }
-    }
-
-    private void updateFade() {
-        Hylde valgtHylde = lvwHylder.getSelectionModel().getSelectedItem();
-        if (valgtHylde != null) {
-            lvwFade.getItems().setAll(valgtHylde.getFade());
-        }
+        //TODO: Update controls
     }
 }
