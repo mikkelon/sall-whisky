e4fe608a50819f84a6e223d5170dc0cff3e6cdb9
Mikkel <mikkelovergaardnielsen@gmail.com>
2023-03-27 18:07:31 +0200

    gjort vinduet til en fast størrelse og centreret alle GridPanes

--- src/gui/StartVindue.java
+++ src/gui/StartVindue.java
@@ -3,6 +3,7 @@ package gui;
 import application.controller.Controller;
 import gui.gui_lagerstyring.LagerstyringPane;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -22,8 +23,8 @@ public class StartVindue extends Application {
         this.initContent(pane);
         stage.setResizable(false);
 
-        //stage.setWidth(500);
-        //stage.setHeight(500);
+        stage.setWidth(900);
+        stage.setHeight(600);
 
         Scene scene = new Scene(pane);
         stage.setScene(scene);
@@ -42,24 +43,28 @@ public class StartVindue extends Application {
         Tab lagerstyringTab = new Tab("Lagerstyring");
         tabPane.getTabs().add(lagerstyringTab);
         LagerstyringPane lagerstyringPane = new LagerstyringPane();
+        lagerstyringPane.setAlignment(Pos.TOP_CENTER);
         lagerstyringTab.setContent(lagerstyringPane);
         lagerstyringTab.setOnSelectionChanged(event -> lagerstyringPane.updateControls());
 
         Tab fadeTab = new Tab("Fade");
         tabPane.getTabs().add(fadeTab);
         FadePane fadePane = new FadePane();
+        fadePane.setAlignment(Pos.TOP_CENTER);
         fadeTab.setContent(fadePane);
         //fadeTab.setOnSelectionChanged(event -> fadePane.updateControls());
 
         Tab destillatTab = new Tab("Destillat");
         tabPane.getTabs().add(destillatTab);
         DestillatPane destillatPane = new DestillatPane();
+        destillatPane.setAlignment(Pos.TOP_CENTER);
         destillatTab.setContent(destillatPane);
         //destillatTab.setOnSelectionChanged(event -> destillatPane.updateControls());
 
         Tab påfyldningTab = new Tab("Påfyldning");
         tabPane.getTabs().add(påfyldningTab);
         PåfyldningPane påfyldningPane = new PåfyldningPane();
+        påfyldningPane.setAlignment(Pos.TOP_CENTER);
         påfyldningTab.setContent(påfyldningPane);
        // påfyldningTab.setOnSelectionChanged(event -> påfyldningPane.updateControls());
 
