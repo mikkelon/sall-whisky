17f5831248977fbbc24d9cc8a2a77834cef35578
Mikkel <mikkelovergaardnielsen@gmail.com>
2023-03-27 16:01:30 +0200

    test af nye Fad metoder samt rettelser i Fad-klassen

--- test/application/model/FadTest.java
+++ test/application/model/FadTest.java
@@ -1,6 +1,10 @@
 package application.model;
+import application.controller.Controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FadTest {
@@ -8,20 +12,28 @@ class FadTest {
     private Lager lager;
     private Hylde hylde;
     private FadLeverandør fadLeverandør;
+    private LocalDate startDato, slutDato;
+    private Destillat destillat;
+    private LocalDate påfyldningsDato;
 
     @BeforeEach
     void setUp() {
-       lager = new Lager("Baldersgade 39", "Sall Whisky Lager", 100.0);
-       hylde = lager.createHylde();
-       fadLeverandør = new FadLeverandør("Garrison Brothers", "USA");
-       fad = new Fad(FadType.BOURBON, 80, fadLeverandør, hylde);
+        lager = new Lager("Baldersgade 39", "Sall Whisky Lager", 100.0);
+        hylde = lager.createHylde();
+        fadLeverandør = new FadLeverandør("Garrison Brothers", "USA");
+        fad = new Fad(FadType.BOURBON, 80, fadLeverandør, hylde);
+        startDato = LocalDate.of(2023, 1, 1);
+        slutDato = LocalDate.of(2023, 1, 2);
+        destillat = new Destillat("77p", "Jens", 61, 2, startDato, slutDato,
+                100, "Kommentar", null);
+        påfyldningsDato = LocalDate.of(2023, 1, 2);
     }
 
     @Test
     void constructorTC1() {
         assertEquals(FadType.BOURBON, fad.getFadType());
         assertEquals(80, fad.getStørrelseILiter());
-        assertEquals(0, fad.getIndeholdtVæskeILiter());
+        assertEquals(0, fad.indeholdtVæskeILiter());
         assertEquals(hylde, fad.getHylde());
         assertEquals(fadLeverandør, fad.getFadLeverandør());
         assertTrue(hylde.getFade().contains(fad));
@@ -42,11 +54,7 @@ class FadTest {
         assertEquals(80, fad.getStørrelseILiter());
     }
 
-    @Test
-    void getSetIndeholdtVæskeILiterTC4() {
-        fad.setIndeholdtVæskeILiter(50);
-        assertEquals(50, fad.getIndeholdtVæskeILiter());
-    }
+    // TC4 er udgået, da getIndeholdtVæskeILiter() er erstattet med indeholdtVæskeILiter()
 
     @Test
     void getHyldeTC5() {
@@ -70,5 +78,109 @@ class FadTest {
     @Test
     void setHyldeFejlTC8() {
         assertThrows(RuntimeException.class, () -> fad.setHylde(null));
+    }
+
+    @Test
+    void addPåfyldningTC9() {
+        Påfyldning påfyldning = new Påfyldning(destillat, fad, "Mikkel", 25, påfyldningsDato);
+        fad.addPåfyldning(påfyldning);
+        assertTrue(fad.getPåfyldninger().contains(påfyldning));
+    }
+
+    @Test
+    void indeholdtVæskeILiterTC10() {
+        Påfyldning påfyldning1 = new Påfyldning(destillat, fad, "Mikkel", 25, påfyldningsDato);
+        Påfyldning påfyldning2 = new Påfyldning(destillat, fad, "Mikkel", 50, påfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+        fad.addPåfyldning(påfyldning2);
+
+        assertEquals(75, fad.indeholdtVæskeILiter());
+    }
+
+    @Test
+    void resterendePladsILiterTC11() {
+        Påfyldning påfyldning1 = new Påfyldning(destillat, fad, "Mikkel", 25, påfyldningsDato);
+        Påfyldning påfyldning2 = new Påfyldning(destillat, fad, "Mikkel", 50, påfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+        fad.addPåfyldning(påfyldning2);
+
+        assertEquals(5, fad.resterendePladsILiter());
+    }
+
+    @Test
+    void forventetFærdigproduceretTC12() {
+        LocalDate nyPåfyldningsDato = LocalDate.of(2023, 1, 3);
+        Påfyldning påfyldning1 = new Påfyldning(destillat, fad, "Mikkel", 25, påfyldningsDato);
+        Påfyldning påfyldning2 = new Påfyldning(destillat, fad, "Mikkel", 50, nyPåfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+        fad.addPåfyldning(påfyldning2);
+
+        LocalDate forventetSlutDato = LocalDate.of(2026, 1, 3);
+        assertEquals(forventetSlutDato, fad.forventetFærdigproduceret());
+    }
+
+    @Test
+    void getAlkoholProcentTC13() {
+        assertEquals(0, fad.getAlkoholProcent());
+    }
+
+    @Test
+    void getAlkoholProcentTC14() {
+        Destillat destillat1 = new Destillat("77p", "Jens", 50, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning1 = new Påfyldning(destillat1, fad, "Mikkel", 1, påfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+
+        assertEquals(50, fad.getAlkoholProcent());
+    }
+
+    @Test
+    void getAlkoholProcentTC15() {
+        Destillat destillat1 = new Destillat("77p", "Jens", 40, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning1 = new Påfyldning(destillat1, fad, "Mikkel", 2, påfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+
+        Destillat destillat2 = new Destillat("77p", "Jens", 60, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning2 = new Påfyldning(destillat2, fad, "Mikkel", 1, påfyldningsDato);
+        fad.addPåfyldning(påfyldning2);
+
+        assertEquals(46.6667, fad.getAlkoholProcent(), 0.0001);
+    }
+
+    @Test
+    void getAlkoholProcentTC16() {
+        Destillat destillat1 = new Destillat("77p", "Jens", 0, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning1 = new Påfyldning(destillat1, fad, "Mikkel", 1, påfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+
+        Destillat destillat2 = new Destillat("77p", "Jens", 100, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning2 = new Påfyldning(destillat2, fad, "Mikkel", 1, påfyldningsDato);
+        fad.addPåfyldning(påfyldning2);
+
+        assertEquals(50, fad.getAlkoholProcent(), 0.0001);
+    }
+
+    @Test
+    void getAlkoholProcentTC17() {
+        Destillat destillat1 = new Destillat("77p", "Jens", 20, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning1 = new Påfyldning(destillat1, fad, "Mikkel", 1, påfyldningsDato);
+        fad.addPåfyldning(påfyldning1);
+
+        Destillat destillat2 = new Destillat("77p", "Jens", 40, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning2 = new Påfyldning(destillat2, fad, "Mikkel", 2, påfyldningsDato);
+        fad.addPåfyldning(påfyldning2);
+
+        Destillat destillat3 = new Destillat("77p", "Jens", 60, 2,
+                startDato, slutDato, 100, "Kommentar", null);
+        Påfyldning påfyldning3 = new Påfyldning(destillat3, fad, "Mikkel", 1, påfyldningsDato);
+        fad.addPåfyldning(påfyldning3);
+
+        assertEquals(40, fad.getAlkoholProcent(), 0.0001);
     }
 }
