788ed6b4124b788b1615e1067045fcbd6cf25709
Mads <mads.soenderstrup@gmail.com>
2023-03-30 08:22:04 +0200

    small changes and javadoc for Whisky

--- src/application/model/Whisky.java
+++ src/application/model/Whisky.java
@@ -2,44 +2,82 @@ package application.model;
 
 import java.util.ArrayList;
 
+/**
+ * Modellerer et whiskyprodukt som kan bestå af flere forskellige aftapninger på fade.
+ */
 public class Whisky {
     private double alkoholProcent;
-    private String betegnelse;
+    private Betegnelse betegnelse;
     private double mængdeVandILiter;
     private String vandAfstamning;
     private String tekstBeskrivelse;
+    private static int antalWhiskyProdukter = 0;
     private int whiskyNr;
     private ArrayList<Aftapning> aftapninger = new ArrayList<>();
 
-    public Whisky(double alkoholProcent, String betegnelse, double mængdeVandILiter, String vandAfstamning, String tekstBeskrivelse, int whiskyNr) {
+    /**
+     * Initialiserer et nyt whiskyprodukt med alkoholprocent, betegnelse, mængde vand i liter, vandafstamning, tekstbeskrivelse og et unikt nummer.
+     * Pre: 0 < alkoholProcent < 100, mængdeVandILiter >= 0, tekstBeskrivelse != null, betegnelse != null, vandAfstamning != null
+     * @param alkoholProcent alkoholprocenten på whiskyproduktet
+     * @param betegnelse betegnelsen på whiskyproduktet
+     * @param mængdeVandILiter mængden af vand i liter der er brugt i whiskyproduktet
+     * @param vandAfstamning afstamningen af vandet der er brugt i whiskyproduktet
+     * @param tekstBeskrivelse tekstbeskrivelsen af whiskyproduktet
+     */
+    public Whisky(double alkoholProcent, Betegnelse betegnelse, double mængdeVandILiter, String vandAfstamning, String tekstBeskrivelse) {
+        antalWhiskyProdukter++;
+        this.whiskyNr = antalWhiskyProdukter;
         this.alkoholProcent = alkoholProcent;
         this.betegnelse = betegnelse;
         this.mængdeVandILiter = mængdeVandILiter;
         this.vandAfstamning = vandAfstamning;
         this.tekstBeskrivelse = tekstBeskrivelse;
-        this.whiskyNr = whiskyNr;
     }
 
+    /**
+     * Returnerer alkoholprocenten på whiskyproduktet.
+     * @return alkoholprocenten på whiskyproduktet
+     */
     public double getAlkoholProcent() {
         return alkoholProcent;
     }
 
-    public String getBetegnelse() {
+    /**
+     * Returnerer betegnelsen på whiskyproduktet.
+     * @return betegnelsen på whiskyproduktet
+     */
+    public Betegnelse getBetegnelse() {
         return betegnelse;
     }
 
+    /**
+     * Returnerer mængden af vand i liter der er brugt i whiskyproduktet.
+     * @return mængden af vand i liter der er brugt i whiskyproduktet
+     */
     public double getMængdeVandILiter() {
         return mængdeVandILiter;
     }
 
+    /**
+     * Returnerer afstamningen af vandet der er brugt i whiskyproduktet.
+     * @return afstamningen af vandet der er brugt i whiskyproduktet
+     */
     public String getVandAfstamning() {
         return vandAfstamning;
     }
 
+    /**
+     * Returnerer tekstbeskrivelsen af whiskyproduktet.
+     * @return tekstbeskrivelsen af whiskyproduktet
+     */
     public String getTekstBeskrivelse() {
         return tekstBeskrivelse;
     }
 
+    /**
+     * Returnerer et unikt nummer på whiskyproduktet.
+     * @return et unikt nummer på whiskyproduktet
+     */
     public int getWhiskyNr() {
         return whiskyNr;
     }
