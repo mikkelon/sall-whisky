690ce01d2b3f7247416a235cdbfa1a3d9588decd
frederikkewp <125652706+frederikkewp@users.noreply.github.com>
2023-04-04 13:15:21 +0200

    JaveFX for WhiskyPane uden funktionalitet (#68)

--- src/gui/WhiskyPane.java
+++ src/gui/WhiskyPane.java
@@ -1,6 +1,7 @@
 package gui;
 
 import application.model.Betegnelse;
+import application.model.Whisky;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
 import javafx.scene.layout.GridPane;
@@ -8,7 +9,6 @@ import javafx.scene.control.*;
 
 
 public class WhiskyPane extends GridPane {
-    private final ComboBox<Betegnelse> cbxBetegnelse;
 
     public WhiskyPane(){
         this.setPadding(new Insets(10));
@@ -16,41 +16,64 @@ public class WhiskyPane extends GridPane {
         this.setVgap(10);
         this.setGridLinesVisible(false);
 
+        Label lblWhisky = new Label("Whisky");
+        this.add(lblWhisky, 0, 0);
+
+        ListView<Whisky> lvwWhiskyer = new ListView<>();
+        lvwWhiskyer.setMinWidth(200);
+        lvwWhiskyer.setMinHeight(300);
+        this.add(lvwWhiskyer,0, 1, 1, 6);
+
         Label lblVandILiter = new Label("Vand (Liter)");
-        this.add(lblVandILiter, 0, 0);
+        this.add(lblVandILiter, 1, 1);
 
         TextField txfVandILiter = new TextField();
-        this.add(txfVandILiter, 0, 1);
+        this.add(txfVandILiter, 1, 2);
 
         Label lblAlkoholprocent = new Label("Alkoholprocent");
-        this.add(lblAlkoholprocent, 0, 2);
+        this.add(lblAlkoholprocent, 1, 3);
 
         TextField txfAlkoholprocent = new TextField();
-        this.add(txfAlkoholprocent, 0, 3);
+        this.add(txfAlkoholprocent, 1, 4);
 
         Label lblBeskrivelse = new Label("Beskrivelse");
-        this.add(lblBeskrivelse, 0, 4);
+        this.add(lblBeskrivelse, 1, 5);
 
-        TextField txfBeskrivelse = new TextField();
-        this.add(txfBeskrivelse, 0, 5);
+        TextArea txaBeskrivelse = new TextArea();
+        txaBeskrivelse.setMinWidth(180);
+        txaBeskrivelse.setMinHeight(100);
+        txaBeskrivelse.setMaxWidth(180);
+        txaBeskrivelse.setMaxHeight(100);
+        GridPane.setValignment(txaBeskrivelse, VPos.TOP);
+        this.add(txaBeskrivelse, 1, 6, 1, 2);
 
         Label lblVandkilde = new Label("Vandkilde");
-        this.add(lblVandkilde, 1, 0);
+        this.add(lblVandkilde, 2, 1);
 
         ComboBox<String> cbxVandKilde = new ComboBox<>();
         GridPane.setValignment(cbxVandKilde, VPos.TOP);
         cbxVandKilde.setMinWidth(150);
         cbxVandKilde.setMaxWidth(150);
-        this.add(cbxVandKilde, 1,1);
+        this.add(cbxVandKilde, 2,2);
 
         Label lblBetegnelse = new Label("Betegnelse");
-        this.add(lblBetegnelse, 1, 2);
+        this.add(lblBetegnelse, 2, 3);
 
-        cbxBetegnelse = new ComboBox<>();
+        ComboBox<Betegnelse> cbxBetegnelse = new ComboBox<>();
         GridPane.setValignment(cbxBetegnelse, VPos.TOP);
         cbxBetegnelse.setMinWidth(150);
         cbxBetegnelse.setMaxWidth(150);
-        this.add(cbxBetegnelse, 1,3);
+        this.add(cbxBetegnelse, 2,4);
+
+        Label lblHistorik = new Label("Historik");
+        this.add(lblHistorik, 3, 0);
+
+        TextArea txaHistorik = new TextArea();
+        txaHistorik.setMinWidth(200);
+        //txaHistorik.setMinHeight(300);
+        txaHistorik.setMaxWidth(200);
+        //txaHistorik.setMaxHeight(300);
+        this.add(txaHistorik, 3, 1, 1 , 6);
 
 
     }
